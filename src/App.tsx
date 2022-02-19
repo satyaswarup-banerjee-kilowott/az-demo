b7487426a3e8f42f13b39c5973e055d7e7964639
satyaswarup-banerjee-kilowott <satyaswarup.banerjee@kilowott.com>
2022-02-19 15:41:51 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -19,13 +19,9 @@ function App() {
 
       }catch(err:any){
 
-        if(err.response){
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.headers);
-        }else{
+        
           console.log(`Error: ${err.message}`)
-        }
+        
 
         
       }
