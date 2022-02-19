3e7d81f597be0b8c0013857c057a43306d2700cb
satyaswarup-banerjee-kilowott <satyaswarup.banerjee@kilowott.com>
2022-02-19 12:24:38 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+// import httpTrigger from '../api/testApi/index'
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    (async function () {
+      const { body } = await( await fetch(`/api/testApi`)).json();
+      console.log(body);
+      setData(body);
+    })();
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello from az.demo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>{data}</div>
+      
     </div>
   );
 }
